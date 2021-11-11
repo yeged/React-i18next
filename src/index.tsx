81240c97262d828b37b6585fbbf5eae2576cf031
yeged <yigityceylan@gmail.com>
2021-11-11 12:03:14 +0300

    React i18n implementation

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import App from './App';
+import i18n from 'i18next';
+import { useTranslation, initReactI18next } from 'react-i18next';
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+    resources: {
+      en: {
+        translation: {
+          'Welcome to React': 'Welcome to React and react-i18next',
+        },
+      },
+      tr: {
+        translation: {
+          'Welcome to React': 'Reacte Hoş geldin ve react-i18next',
+        },
+      },
+    },
+    lng: 'en', // if you're using a language detector, do not define the lng option
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    },
+  });
+function App() {
+  const { t } = useTranslation();
+  return <h2>{t('Welcome to React')}</h2>;
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +40,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
