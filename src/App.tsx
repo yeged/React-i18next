2de396a5df08112873420ad4247ccc34efd60f53
yeged <yigityceylan@gmail.com>
2021-11-11 14:43:05 +0300

    Import i18next & Added change language button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
 import { useTranslation } from 'react-i18next';
+import i18next from 'i18next';
 
 function App() {
   const { t } = useTranslation();
+
+  const changeLanguage = (lang: string): void => {
+    i18next.changeLanguage(lang);
+  };
   return (
     <div>
+      <button onClick={() => changeLanguage('en')}>EN</button>
+      <button onClick={() => changeLanguage('tr')}>TR</button>
+      <button onClick={() => changeLanguage('de')}>DE</button>
       <h2>{t('title', { name: 'Yiğit' })}</h2>
       <p>{t('description.part1')}</p>
     </div>
