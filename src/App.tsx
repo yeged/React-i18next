e981f5ae58b8460c5be6b7d40e06b0f276c375ab
yeged <yigityceylan@gmail.com>
2021-11-11 18:25:06 +0300

    Cookies and languages dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,57 @@
 import './App.css';
 import { Trans, useTranslation } from 'react-i18next';
+import React, { useState } from 'react';
+import cookies from 'js-cookie';
 
 function App() {
   const { t, i18n } = useTranslation();
 
+  const [selectedLang, setSelectedLang] = useState<string>(
+    cookies.get('i18next') || 'en',
+  );
+
+  const languages = [
+    {
+      code: 'en',
+      name: 'English',
+      country_code: 'gb',
+    },
+    {
+      code: 'tr',
+      name: 'Türkçe',
+      country_code: 'tr',
+    },
+    {
+      code: 'de',
+      name: 'Deutsch',
+      country_code: 'de',
+    },
+  ];
   const age: number = 23;
   const messages: string[] = ['sa', 'as'];
 
-  const changeLanguage = (lang: string): void => {
-    i18n.changeLanguage(lang);
+  const changeLanguage = (
+    e: React.ChangeEvent<HTMLSelectElement>,
+  ): void => {
+    setSelectedLang(e.target.value);
+    i18n.changeLanguage(e.target.value);
   };
   return (
     <div>
-      <button onClick={() => changeLanguage('en')}>EN</button>
-      <button onClick={() => changeLanguage('tr')}>TR</button>
-      <button onClick={() => changeLanguage('de')}>DE</button>
+      <select
+        value={selectedLang}
+        onChange={changeLanguage}
+        onBlur={changeLanguage}
+      >
+        {languages.map(({ code, name, country_code }) => {
+          return (
+            <option key={country_code} value={code}>
+              {name}
+            </option>
+          );
+        })}
+      </select>
+
       <h2>{t('title', { name: 'Yiğit' })}</h2>
       <p>{t('description.part1')}</p>
       <Trans i18nKey="description.part2" />
