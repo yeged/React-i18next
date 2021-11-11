8f3932533ee0b465a61e914995ce426b482f72a2
yeged <yigityceylan@gmail.com>
2021-11-11 12:07:26 +0300

    Defined lang option

--- src/index.tsx
+++ src/index.tsx
@@ -23,7 +23,7 @@ i18n
         },
       },
     },
-    lng: 'en', // if you're using a language detector, do not define the lng option
+    lng: document.querySelector('html')!.lang, // if you're using a language detector, do not define the lng option
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
