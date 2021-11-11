981d7aab2a6a2871bad619292ea20c395334f286
yeged <yigityceylan@gmail.com>
2021-11-11 11:57:53 +0300

    Created React-ts Project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  return <div className="App">Hello world Language Package</div>;
+}
+
+export default App;
