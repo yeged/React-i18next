f62a6560212273b5104e3847e9930eb4dbdab7a7
yeged <yigityceylan@gmail.com>
2022-02-09 21:41:01 +0300

    ISV-7: Feat 7

--- src/components/SimpleMarkdown.tsx
+++ src/components/SimpleMarkdown.tsx
@@ -66,6 +66,7 @@ const SimpleMarkdown = () => {
     <div>
       <h1>Hocam ekliyoruz feat messages</h1>
       <h2>feat 1 </h2>
+      <h3>feat 7 test 7 olacak</h3>
       <button onClick={() => setMarkdown(t('gfm'))}>
         Plugin: remark-gfm
       </button>
